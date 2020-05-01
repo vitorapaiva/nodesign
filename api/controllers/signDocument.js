15701320444bd6bb26ff556265b1b3fcae9d92b1
Vitor Paiva <vitor.albuquerque@veus.com.br>
2020-05-01 18:56:45 -0300

    Envio de desenvolvimento

--- api/controllers/signDocument.js
+++ api/controllers/signDocument.js
@@ -1,10 +1,21 @@
+const signer = require('node-signpdf');
+
 module.exports = () => {
   const controller = {};
 
   controller.signDocument = (request, response) => {
-    let responseData = {};
-    responseData.ok='ok';
-    response.status(200).json(responseData);
+    let file=Buffer.from(request.body.file, 'base64').toString();
+    let certificate=Buffer.from(request.body.certificate, 'base64').toString();
+    let password=request.body.password;
+    let pdfBuffer = signer.sign(
+        file,
+        certificate,
+        {passphrase: password},
+    );
+    let result={};
+    result.file=Buffer.from(pdfBuffer).toString('base64');
+    result.status='OK';
+    response.status(200).json(result);
   }
 
   return controller;
